d350ecbe3d9213ee4c6c6d171a5498ff5f001352
Taluna Mendes <taluna.mendes_thoughtworks@prestador.globo>
2022-10-04 20:52:00 -0300

    Add terraform scripts

---
+++ infra/variables.tf
@@ -0,0 +1,9 @@
+variable "s3_objects_names" {
+  type = list
+  default = ["small-files", "raw-json","ingested-json", "parquet"]
+}
+
+//variable "s3_objects_names" {
+//  type = list
+//  default = ["ingested-json", "parquet"]
+//}

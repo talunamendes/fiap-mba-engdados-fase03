318a698b3b2f1ebc7676b1b094c922c61bf93c1e
Taluna Mendes <88059577+talunamendes@users.noreply.github.com>
2022-10-17 04:56:32 -0300

    deleting old directory

--- infra/sqs_dlq.tf
+++
@@ -1,5 +0,0 @@
-module "sqs-with-dlq" {
-  count= length(var.sqs_names)
-  source  = "git::git@github.com:damacus/terraform-aws-sqs-with-dlq.git"
-  name = var.sqs_names[count.index]
-}
--- infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "s3_objects_names" {
-  type = list
-  default = ["small-files", "raw-json","ingested-json", "parquet"]
-}
-
-variable "sqs_names" {
-  type = list
-  default = ["tf-csv-to-json", "tf-json-to-firehose"]
-}

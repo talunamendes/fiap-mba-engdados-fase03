3b29c0954d50a350a9f4fb2850ab421b75c01235
Taluna Mendes <taluna.mendes_thoughtworks@prestador.globo>
2022-10-17 07:20:44 -0300

    remove comments from files

--- 00-instalacao/terraform/s3.tf
+++ 00-instalacao/terraform/s3.tf
@@ -1,18 +1,5 @@
 
 #---- AWS S3
-/*
-resource "aws_s3_bucket" "bucket_s3" {
-  bucket = "s3-fiap-grupo-o"
-}
-
-resource "aws_s3_bucket_acl" "bucket_s3_acl" {
-  bucket = aws_s3_bucket.bucket_s3.id
-  acl    = "private"
-  depends_on = [
-    bucket_s3
-  ]
-}
-*/
 
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
